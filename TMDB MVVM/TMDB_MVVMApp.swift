ded885cc0efa0c66083acbd7c89a27e66b67fb5e
Aleksandar Milidrag <salkaorama@gmail.com>
2024-12-21 11:57:12 +0100

    Initial Commit

---
+++ TMDB MVVM/TMDB_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_MVVMApp.swift
+//  TMDB MVVM
+//
+//  Created by Aleksandar Milidrag on 21. 12. 2024..
+//
+
+import SwiftUI
+
+@main
+struct TMDB_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
