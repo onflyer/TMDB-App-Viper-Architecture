a56454bf91d276d0bff2fd9a00118bed6758a962
Aleksandar Milidrag <salkaorama@gmail.com>
2024-12-21 14:15:13 +0100

    Update MovieServiceMock.swift

--- TMDB MVVM/Services/Movies/MovieServiceMock.swift
+++ TMDB MVVM/Services/Movies/MovieServiceMock.swift
@@ -9,8 +9,25 @@ import Foundation
 
 struct MovieServiceMock: MovieService {
     
+    let avatars: [Movie]
+    let delay: Double
+    let showError: Bool
+    
+    init(avatars: [Movie] = Movie.mocks(), delay: Double = 1, showError: Bool = false) {
+        self.avatars = avatars
+        self.delay = delay
+        self.showError = showError
+    }
+    
     func getNowPlayingMovies(page: Int) async throws -> [Movie] {
-        try? await Task.sleep(for: .seconds(1))
-        return Movie.mocks()
+        try await Task.sleep(for: .seconds(delay))
+        try tryShowError()
+        return avatars
+    }
+    
+    private func tryShowError() throws {
+        if showError {
+            throw URLError(.unknown)
+        }
     }
 }
