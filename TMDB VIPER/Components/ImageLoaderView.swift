8f93f318f076101bfa0e59047a35765088f51f52
Aleksandar Milidrag <salkaorama@gmail.com>
2024-12-23 11:42:49 +0100

    added image loader view

---
+++ TMDB VIPER/Components/ImageLoaderView.swift
@@ -0,0 +1,42 @@
+//
+//  ImageLoaderView.swift
+//  TMDB VIPER
+//
+//  Created by Aleksandar Milidrag on 23. 12. 2024..
+//
+
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
+    var forceTransitionAnimation: Bool = false
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0.5)
+            .overlay(
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
+            .ifSatisfiedCondition(forceTransitionAnimation) { content in
+                content
+                    .drawingGroup()
+            }
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .frame(width: 200, height: 200)
+        .anyButton(.highlight) {
+            
+        }
+}
---
+++ TMDB VIPER/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TMDB VIPER
+//
+//  Created by Aleksandar Milidrag on 23. 12. 2024..
+//
+
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
