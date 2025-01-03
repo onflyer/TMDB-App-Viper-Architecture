6fff35631b34a98cf68fe130295032f95a761549
Aleksandar Milidrag <salkaorama@gmail.com>
2025-01-03 20:01:13 +0100

    Update DetailView.swift

--- TMDB VIPER/DetailView/DetailView.swift
+++ TMDB VIPER/DetailView/DetailView.swift
@@ -17,28 +17,30 @@ struct DetailView: View {
     let delegate: DetailViewDelegate
 
     var body: some View {
-        ZStack {
-            ImageLoaderView(urlString: presenter.movie?.backdropURLString ?? "No image")
-                .aspectRatio(16/9, contentMode: .fit)
-                .navigationTitle(presenter.movie?.title ?? "No title")
-                .navigationBarTitleDisplayMode(.inline)
-        }
-        .overlay(alignment: .bottomLeading) {
-            MovieCellView(title: "", imageName: presenter.movie?.posterURLString ?? "No image")
-                .frame(width: 120, height: 180)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(.white, lineWidth: 1)
-                }
-                .offset(x: 20, y: 90)
-                
-        }
         VStack {
-            Text("movie")
-        }
-        .frame(maxHeight: .infinity)
-        .task {
-            await presenter.loadSingleMovie(id: delegate.movieId)
+            ZStack {
+                ImageLoaderView(urlString: presenter.movie?.backdropURLString ?? "No image")
+                    .aspectRatio(16/9, contentMode: .fit)
+                    .navigationTitle(presenter.movie?.title ?? "No title")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .zIndex(1)
+            .overlay(alignment: .bottomLeading) {
+                MovieCellView(title: "", imageName: presenter.movie?.posterURLString ?? "No image")
+                    .frame(width: 120, height: 180)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(.white, lineWidth: 1)
+                    }
+                    .offset(x: 20, y: 90)
+            }
+            ScrollView {
+                
+            }
+            .frame(maxHeight: .infinity)
+            .task {
+                await presenter.loadSingleMovie(id: delegate.movieId)
+            }
         }
     }
 }
