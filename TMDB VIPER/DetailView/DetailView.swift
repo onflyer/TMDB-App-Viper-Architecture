48a410cb45bc9076c48c61c6c1b8983235a20efb
Aleksandar Milidrag <salkaorama@gmail.com>
2025-01-03 19:32:21 +0100

    update detail view

--- TMDB VIPER/DetailView/DetailView.swift
+++ TMDB VIPER/DetailView/DetailView.swift
@@ -17,10 +17,26 @@ struct DetailView: View {
     let delegate: DetailViewDelegate
 
     var body: some View {
-        VStack {
-            Text(presenter.movie?.title ?? "No title")
+        ZStack {
+            ImageLoaderView(urlString: presenter.movie?.backdropURLString ?? "No image")
+                .aspectRatio(16/9, contentMode: .fit)
                 .navigationTitle(presenter.movie?.title ?? "No title")
+                .navigationBarTitleDisplayMode(.inline)
         }
+        .overlay(alignment: .bottomLeading) {
+            MovieCellView(title: "", imageName: presenter.movie?.posterURLString ?? "No image")
+                .frame(width: 120, height: 180)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.white, lineWidth: 1)
+                }
+                .offset(x: 20, y: 90)
+                
+        }
+        VStack {
+            Text("movie")
+        }
+        .frame(maxHeight: .infinity)
         .task {
             await presenter.loadSingleMovie(id: delegate.movieId)
         }
