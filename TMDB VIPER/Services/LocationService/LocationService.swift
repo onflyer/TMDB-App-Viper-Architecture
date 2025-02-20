5f36cb7e41626edfd293847d814fd70514853f89
Aleksandar Milidrag <salkaorama@gmail.com>
2025-02-20 16:57:00 +0100

    added location service protocol

--- TMDB VIPER/Services/LocationService/LocationService.swift
+++ TMDB VIPER/Services/LocationService/LocationService.swift
@@ -1,72 +1,14 @@
 //
-//  TheatreLocationService.swift
+//  LocationService.swift
 //  TMDB VIPER
 //
-//  Created by Aleksandar Milidrag on 18. 2. 2025..
+//  Created by Aleksandar Milidrag on 20. 2. 2025..
 //
 
 import MapKit
 
-final class LocationManager: NSObject, ObservableObject {
-    private let locationManager = CLLocationManager()
-    
-    var region = MKCoordinateRegion()
-    var userLocation: CLLocation? = nil
-    
-    override init() {
-        super.init()
-        
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.setup()
-    }
-    
-    func setup() {
-        switch locationManager.authorizationStatus {
-        //If we are authorized then we request location just once, to center the map
-        case .authorizedWhenInUse:
-            locationManager.requestLocation()
-        //If we don´t, we request authorization
-        case .notDetermined:
-            locationManager.startUpdatingLocation()
-            locationManager.requestWhenInUseAuthorization()
-        default:
-            break
-        }
-    }
-    
-    func searchLocations(query: String) async throws -> MKLocalSearch.Response {
-            let request = MKLocalSearch.Request()
-            request.region = region
-            request.naturalLanguageQuery = query
-            let response = MKLocalSearch(request: request)
-            let results = try await response.start()
-            return results
-    }
-    
-    
-    
-    
-}
+protocol LocationService {
+    func searchLocations(query: String) async throws -> MKLocalSearch.Response
+    func getUserLocation() async throws -> CLLocation?
 
-extension LocationManager: CLLocationManagerDelegate {
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        guard .authorizedWhenInUse == manager.authorizationStatus || manager.authorizationStatus == .authorizedAlways else { return }
-        locationManager.requestLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Something went wrong: \(error)")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locationManager.stopUpdatingLocation()
-        userLocation = locations.last
-        locations.last.map {
-            region = MKCoordinateRegion(
-                center: $0.coordinate,
-                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-            )
-        }
-    }
 }
---
+++ TMDB VIPER/Services/LocationService/LocationServiceProd.swift
@@ -0,0 +1,74 @@
+//
+//  TheatreLocationService.swift
+//  TMDB VIPER
+//
+//  Created by Aleksandar Milidrag on 18. 2. 2025..
+//
+
+import MapKit
+
+final class LocationServiceProd: NSObject {
+    private let locationManager = CLLocationManager()
+    
+    var region = MKCoordinateRegion()
+    var userLocation: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.setup()
+    }
+    
+    func setup() {
+        switch locationManager.authorizationStatus {
+        //If we are authorized then we request location just once, to center the map
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        //If we don´t, we request authorization
+        case .notDetermined:
+            locationManager.startUpdatingLocation()
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+    
+    func searchLocations(query: String) async throws -> MKLocalSearch.Response {
+            let request = MKLocalSearch.Request()
+            request.region = region
+            request.naturalLanguageQuery = query
+            let response = MKLocalSearch(request: request)
+            let results = try await response.start()
+            return results
+    }
+    
+    func getUserLocation() async throws -> CLLocation? {
+        return userLocation
+    }
+    
+    
+}
+
+extension LocationServiceProd: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard .authorizedWhenInUse == manager.authorizationStatus || manager.authorizationStatus == .authorizedAlways else { return }
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Something went wrong: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        userLocation = locations.last
+        locations.last.map {
+            region = MKCoordinateRegion(
+                center: $0.coordinate,
+                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+        }
+    }
+}
