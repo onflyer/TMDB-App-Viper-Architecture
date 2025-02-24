60cecfb15be3615e1c2d67c05c1c1eba3a5b159f
Aleksandar Milidrag <salkaorama@gmail.com>
2025-02-24 19:45:45 +0100

    deleted unused examples

--- TMDB VIPER/Services/LocationService/LocationService.swift
+++ TMDB VIPER/Services/LocationService/LocationService.swift
@@ -7,20 +7,6 @@
 
 import MapKit
 
-protocol LocationService {
-    func searchLocations(query: String) async throws -> MKLocalSearch.Response
-    func getUserLocation() async throws -> CLLocation?
-    
-}
-
-protocol LocationService2 {
-    var authorizationStatus: CLAuthorizationStatus { get }
-    var delegateEvents: AsyncStream<DelegateEvent> { get }
-    func requestWhenInUseAuthorization()
-    func requestLocation()
-    func searchLocations(query: String, region: MKCoordinateRegion) async throws -> [MKMapItem]
-}
-
 protocol LocationService3 {
     func getAuthorizationStatus() async -> CLAuthorizationStatus
     func requestLocation() async throws -> CLLocation
