e7368f073c80b88ba337796c18cf00dec53c801a
Aleksandar Milidrag <salkaorama@gmail.com>
2025-08-03 22:15:55 +0200

    cleanup

--- TMDB VIPER/Services/Network/NetworkService/NetworkService.swift
+++ TMDB VIPER/Services/Network/NetworkService/NetworkService.swift
@@ -1,16 +1,37 @@
 //
 //  NetworkService.swift
-//  TMDB App
+//  TMDB_VIPER
 //
-//  Created by Aleksandar Milidrag on 3/13/24.
+//  Created by Aleksandar Milidrag on 21. 12. 2024..
 //
 
 import Foundation
 
-// MARK: - Request Manager Protocol -
+ final class NetworkService: NetworkServiceProtocol {
+    
+    private let session: URLSession
+    private let decoder: JSONDecoder
 
-public protocol NetworkServiceProtocol {
-    func makeRequest(request: URLComponentsProtocol) async throws -> Data
-    func makeRequest<T: Decodable>(request: URLComponentsProtocol, responseType: T.Type) async throws -> T
+    public init(session: URLSession = .shared, decoder: JSONDecoder = JSONDecoder()) {
+        self.session = session
+        self.decoder = decoder
+    }
+
+    /// Sends a request built from URLComponentsProtocol, returns raw Data
+     func makeRequest(request: URLComponentsProtocol) async throws -> Data {
+        let urlRequest = try request.request()
+        let (data, response) = try await session.data(for: urlRequest)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.invalidResponse
+        }
+        return data
+    }
+
+    /// Sends a request and decodes response into a Decodable model
+     func makeRequest<T: Decodable>(request: URLComponentsProtocol, responseType: T.Type) async throws -> T {
+        let data = try await makeRequest(request: request)
+        return try decoder.decode(T.self, from: data)
+    }
 }
-
---
+++ TMDB VIPER/Services/Network/NetworkService/NetworkServiceProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkServiceProtocol.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 3/13/24.
+//
+
+import Foundation
+
+// MARK: - Request Manager Protocol -
+
+public protocol NetworkServiceProtocol {
+    func makeRequest(request: URLComponentsProtocol) async throws -> Data
+    func makeRequest<T: Decodable>(request: URLComponentsProtocol, responseType: T.Type) async throws -> T
+}
+
