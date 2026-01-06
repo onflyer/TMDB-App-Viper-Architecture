c0d1fe2645949cf9dbbbf845888c940fb7125c59
Aleksandar Milidrag <salkaorama@gmail.com>
2026-01-06 15:52:23 +0100

    fixed modal

--- TMDB VIPER/ImageModalView/ImageModalView.swift
+++ TMDB VIPER/ImageModalView/ImageModalView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ImageModalViewDelegate {
-    var urlString:String = Constants.randomImage
+    var urlString: String = Constants.randomImage
     var onDismiss: () -> Void = {}
 }
 
@@ -17,12 +17,39 @@ struct ImageModalView: View {
     
     var body: some View {
         ZStack {
-            ImageLoaderView(urlString: delegate.urlString)
-                .scaledToFit()
+            // Tappable background to dismiss
+            Color.black.opacity(0.001)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    delegate.onDismiss()
+                }
+            
+            // Image with poster aspect ratio (2:3)
+            ImageLoaderView(urlString: delegate.urlString, resizingMode: .fit)
+                .aspectRatio(2/3, contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .shadow(color: .black.opacity(0.5), radius: 20)
+                .overlay(alignment: .topTrailing) {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(.white.opacity(0.9))
+                        .shadow(radius: 4)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            delegate.onDismiss()
+                        }
+                        .offset(x: 5, y: -35)
+                }
+                .padding(.horizontal, 20)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    ImageModalView(delegate: ImageModalViewDelegate(urlString: Constants.randomImage))
+    ZStack {
+        Color.black.opacity(0.7)
+            .ignoresSafeArea()
+        ImageModalView(delegate: ImageModalViewDelegate(urlString: "https://image.tmdb.org/t/p/w500/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg"))
+    }
 }
